c6a835730680df6b306913d8f64bd1a25bc2670f
SJ_kelvin <kelvin.tung@hotmail.com>
2024-12-18 16:54:40 +0800

    mac fix gitignore(.env .log)

--- src/main/java/com/example/mountaineerback/model/dto/UserDTO.java
+++ src/main/java/com/example/mountaineerback/model/dto/UserDTO.java
@@ -1,8 +1,14 @@
 package com.example.mountaineerback.model.dto;
 
+import com.example.mountaineerback.model.entity.Order;
+import com.example.mountaineerback.model.entity.Trip;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
 
 @Data
 @AllArgsConstructor
@@ -11,6 +17,11 @@ public class UserDTO {
     private Long id;
     private String username;
     private String password;
+    private String trueName;
     private String email;
     private String role;
+    private LocalDateTime createdAt;
+    private List<Order> orders;
+    private List<Trip> createdTrips;
+    private Set<Trip> joinedTrips;
 }
--- src/main/java/com/example/mountaineerback/model/enums/USER_ROLE.java
+++ src/main/java/com/example/mountaineerback/model/enums/USER_ROLE.java
@@ -1,5 +1,5 @@
 package com.example.mountaineerback.model.enums;
 
 public enum USER_ROLE {
-    ROLE_GUEST,ROLE_MEMBER,ROLE_ADMIN
+    UNKNOWN,ROLE_GUEST,ROLE_MEMBER,ROLE_ADMIN
 }
