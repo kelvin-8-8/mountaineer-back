b8ed28eff08c0e6400b31981620d0c9d86fd96af
SJ_kelvin <kelvin.tung@hotmail.com>
2024-12-23 01:32:14 +0800

    improve

--- src/main/java/com/example/mountaineerback/model/request/RegisterRequest.java
+++ src/main/java/com/example/mountaineerback/model/request/RegisterRequest.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RegisterRequest {
     private String username;
+    private String trueName;
     private String password;
     private String email;
 }
